3e156c6265a95eb05ea80fc3b829ba93f8ff7b79
Agsgt1 <138738952+Agsgt1@users.noreply.github.com>
2024-02-21 20:34:28 -0600

    Create math.js

---
+++ math.js
@@ -0,0 +1,36 @@
+function dataload(x) {
+  datav = x
+}
+
+function dataunload() {
+  datav = 0
+}
+
+function data() {
+  return datav
+}
+
+function add(x, y) {
+  return x + y
+}
+
+function sub(x, y) {
+  return x - y
+}
+
+function multi(x, y) {
+  return x * y
+}
+
+function div(x, y) {
+  return x / y
+}
+
+function exp(x, y) {
+  return x ** y
+}
+
+dataload(exp(30, 8))
+d = data()
+dataunload()
+alert(add(multi(d, 7), div(d, exp(5, d))))
